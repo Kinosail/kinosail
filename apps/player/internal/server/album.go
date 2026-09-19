20b8d7091c43e1aad391894f28f08728279b8196
MikeO7 <7740954+MikeO7@users.noreply.github.com>
2026-09-19 12:39:13 -0600

    Format player album adapter

--- apps/player/internal/server/album.go
+++ apps/player/internal/server/album.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	detailSources = catalog.MustDetailSources(catalog.DetailPresentation{
-	Product: "Kinosail Player", ThemeVersion: "cinema-1", StyleVersion: "electric-15",
+		Product: "Kinosail Player", ThemeVersion: "cinema-1", StyleVersion: "electric-15",
 	})
 	detailPages = catalog.NewDetailHTTP(
 		newLocalizedTemplate("album", detailSources.Album),
