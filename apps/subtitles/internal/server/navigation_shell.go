15bfc2cd86f3f5afac37413a281eb68f1251c1c3
MikeO7 <7740954+MikeO7@users.noreply.github.com>
2026-09-26 04:02:46 -0600

    Keep subtitle cleanup labels readable in WebKit

--- apps/subtitles/internal/server/navigation_shell.go
+++ apps/subtitles/internal/server/navigation_shell.go
@@ -26,7 +26,7 @@ func saveNavigation(settings *settingsStore) http.HandlerFunc {
 	return settings.navigationController().Handler(localizedError)
 }
 
-const applicationCSSVersion = "cinema-4"
+const applicationCSSVersion = "cinema-5"
 
 var applicationNavigationView = newLocalizedTemplate("application-navigation", subtitleAppHeader)
 
